3ee1d37f23e3ad140ff45b9b7760e8ad70e1eaaa
Sumnor <rodipoltavskyi@gmail.com>
2025-10-14 20:23:46 +0200

    Update war-era-prototype.py

--- war-era-prototype.py
+++ war-era-prototype.py
@@ -2,6 +2,7 @@ import os, json, asyncio, aiohttp, urllib.parse
 from typing import Optional, Dict, Any, List
 from datetime import datetime
 import discord
+from discord import app_commands
 from discord.ext import commands, tasks
 from discord.ui import View, Button, Modal, TextInput
 
